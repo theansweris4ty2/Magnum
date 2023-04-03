044ca102e317c9eec8bbd3695b8603b9d8f69bde
theansweris4ty2 <105891937+theansweris4ty2@users.noreply.github.com>
2023-04-02 19:49:59 -0700

    Update browser-app.js

--- public/browser-app.js
+++ public/browser-app.js
@@ -15,19 +15,18 @@ const changeSlide = (slide) => {
     s.style.transform = `translateX(${(i - slide) * 100}%)`
   })
 }
-if (window.location.href === 'https://magnum-towers.netlify.app') {
   nextSlideBtn.addEventListener('click', () => {
     currentSlide === maxSlide - 1 ? (currentSlide = 0) : currentSlide++
     changeSlide(currentSlide)
   })
-}
-if (window.location.href === 'https://magnum-towers.netlify.app') {
+
+
   prevSlideBtn.addEventListener('click', () => {
     currentSlide === 0 ? (currentSlide = maxSlide - 1) : currentSlide--
     changeSlide(currentSlide)
     console.log(currentSlide)
   })
-}
+
 
 const sendFormInfo = async () => {
   const dataArray = [...new FormData(form)]
